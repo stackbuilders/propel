1208697c0bde3cd59995ad07d27f2e2a42a857af
Justin Leitgeb <justin@stackbuilders.com>
2011-05-03 00:37:23 -0500

    Reorganize opts

--- lib/propel/option_parser.rb
+++ lib/propel/option_parser.rb
@@ -16,29 +16,29 @@ module Propel
     def parser(options)
       ::OptionParser.new do |parser|
         parser.banner = "Usage: propel [options]\n\n"
-        
-        parser.on('-s', '--status-url STATUS_URL', 'Location of build status feed') do |build_status_url|
-          options[:status_url] = build_status_url
+
+        parser.on('-c', '--[no-]color', '--[no-]colour', 'Turn on or off colored output. Color is off by default.') do |o|
+          options[:color] = o
         end
 
         parser.on('-f', '--fix-ci', 'Use when fixing the CI build') do |o|
           options[:fix_ci] = o
         end
 
+        parser.on('-q', '--[no-]quiet', 'Silences normal output messages.') do |o|
+          options[:quiet] = o
+        end
+
         parser.on('-r', '--[no-]rebase', 'Use pull with --rebase.  Rebase is used by default.') do |o|
           options[:rebase] = o
         end
 
-        parser.on('-c', '--[no-]color', '--[no-]colour', 'Turn on or off colored output. Color is off by default.') do |o|
-          options[:color] = o
+        parser.on('-s', '--status-url STATUS_URL', 'Location of build status feed') do |build_status_url|
+          options[:status_url] = build_status_url
         end
 
         parser.on('-w', '--[no-]wait', 'Waits for fixes to remote build.') do |o|
           options[:wait] = o
-        end
-
-        parser.on('-q', '--[no-]quiet', 'Silences normal output messages.') do |o|
-          options[:quiet] = o
         end
       end
     end
